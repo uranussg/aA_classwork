b461b42da1422362628dffce1a72564343b29e9f
uranussg <uranussg@gmail.com>
2020-01-19 11:13:46 -0800

    self join

--- W5D2/SQLzoo/import_db.sh
+++ W5D2/SQLzoo/import_db.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-dropdb sqlzoo
+#dropdb sqlzoo
 createdb sqlzoo
 psql sqlzoo < data/create_tables.sql
--- W5D2/SQLzoo/lib/09_self_joins.rb
+++ W5D2/SQLzoo/lib/09_self_joins.rb
@@ -39,7 +39,19 @@ end
 def lrt_stops
   # Give the id and the name for the stops on the '4' 'LRT' service.
   execute(<<-SQL)
-  SELECT
+  SELECT DISTINCT
+    stopa.id, stopa.name 
+  FROM 
+    stops stopa
+  JOIN
+    routes a ON stopa.id = a.stop_id
+  JOIN  
+    routes b ON a.stop_id = b.stop_id
+  JOIN
+    stops stopb ON stopb.id = b.stop_id
+    
+  WHERE
+    a.company = 'LRT' AND b.num = '4'
   SQL
 end
 
@@ -62,6 +74,18 @@ def connecting_routes
   # that link these stops have a count of 2. Add a HAVING clause to restrict
   # the output to these two routes.
   execute(<<-SQL)
+  SELECT
+    company,
+    num,
+    COUNT(*)
+  FROM
+    routes
+  WHERE
+    stop_id = 149 OR stop_id = 53
+  GROUP BY
+    company, num
+  HAVING
+    COUNT(*) = 2
   SQL
 end
 
@@ -84,6 +108,17 @@ def cl_to_lr
   # Craiglockhart, without changing routes. Change the query so that it
   # shows the services from Craiglockhart to London Road.
   execute(<<-SQL)
+  SELECT
+    a.company,
+    a.num,
+    a.stop_id,
+    b.stop_id
+  FROM
+    routes a
+  JOIN
+    routes b ON (a.company = b.company AND a.num = b.num)
+  WHERE
+    a.stop_id = 53 AND b.stop_id = 149
   SQL
 end
 
@@ -111,6 +146,21 @@ def cl_to_lr_by_name
   # number. Change the query so that the services between 'Craiglockhart' and
   # 'London Road' are shown.
   execute(<<-SQL)
+  SELECT
+    a.company,
+    a.num,
+    stopa.name,
+    stopb.name
+  FROM
+    routes a
+  JOIN
+    routes b ON (a.company = b.company AND a.num = b.num)
+  JOIN
+    stops stopa ON (a.stop_id = stopa.id)
+  JOIN
+    stops stopb ON (b.stop_id = stopb.id)
+  WHERE
+    stopa.name = 'Craiglockhart' AND stopb.name = 'London Road'
   SQL
 end
 
@@ -118,6 +168,15 @@ def haymarket_and_leith
   # Give the company and num of the services that connect stops
   # 115 and 137 ('Haymarket' and 'Leith')
   execute(<<-SQL)
+  SELECT DISTINCT
+  a.company,
+  a.num
+  FROM
+    routes a
+  JOIN
+    routes b ON (a.company = b.company AND a.num = b.num)
+  WHERE
+    a.stop_id = 115 AND b.stop_id = 137
   SQL
 end
 
@@ -125,6 +184,19 @@ def craiglockhart_and_tollcross
   # Give the company and num of the services that connect stops
   # 'Craiglockhart' and 'Tollcross'
   execute(<<-SQL)
+  SELECT
+  a.company,
+  a.num
+  FROM
+    routes a
+  JOIN
+    routes b ON (a.company = b.company AND a.num = b.num)
+  JOIN
+    stops stopa ON (a.stop_id = stopa.id)
+  JOIN
+    stops stopb ON (b.stop_id = stopb.id)
+  WHERE
+    stopa.name = 'Craiglockhart' AND stopb.name = 'Tollcross'
   SQL
 end
 
@@ -133,6 +205,20 @@ def start_at_craiglockhart
   # by taking one bus, including 'Craiglockhart' itself. Include the stop name,
   # as well as the company and bus no. of the relevant service.
   execute(<<-SQL)
+  SELECT DISTINCT
+  stopb.name,
+  a.company,
+  a.num
+  FROM
+    routes a
+  JOIN
+    routes b ON (a.company = b.company AND a.num = b.num)
+  JOIN
+    stops stopa ON (a.stop_id = stopa.id)
+  JOIN
+    stops stopb ON (b.stop_id = stopb.id)
+  WHERE
+    stopa.name = 'Craiglockhart' 
   SQL
 end
 
@@ -141,5 +227,30 @@ def craiglockhart_to_sighthill
   # Sighthill. Show the bus no. and company for the first bus, the name of the
   # stop for the transfer, and the bus no. and company for the second bus.
   execute(<<-SQL)
+  SELECT DISTINCT
+  a.num,
+  a.company,
+
+  stopb.name,
+  c.num,
+  c.company
+
+  FROM
+    routes a
+  JOIN
+    routes b ON (a.company = b.company AND a.num = b.num)
+  JOIN
+    stops stopa ON (a.stop_id = stopa.id)
+  JOIN
+    stops stopb ON (b.stop_id = stopb.id)
+  JOIN
+    routes c ON (c.stop_id = b.stop_id)
+  JOIN
+    routes d ON (c.company = d.company AND c.num = d.num)
+  JOIN
+    stops stopd ON (d.stop_id = stopd.id)
+
+  WHERE
+    stopa.name = 'Craiglockhart' AND  stopd.name = 'Sighthill' 
   SQL
 end
