da9118dda2bb328e201872e523d8b495716f63c8
uranussg <uranussg@gmail.com>
2020-01-19 15:07:41 -0800

    sqlzoo done

--- W5D2/SQLzoo/lib/04a_countries_bonus.rb
+++ W5D2/SQLzoo/lib/04a_countries_bonus.rb
@@ -55,5 +55,67 @@ def large_neighbors
   # Some countries have populations more than three times that of any of their
   # neighbors (in the same continent). Give the countries and continents.
   execute(<<-SQL)
+  SELECT 
+    candidate.name, candidate.continent
+  FROM
+    (SELECT
+      c.continent, c.name, c.population
+    FROM
+      countries c
+    JOIN
+    (SELECT
+      continent, MAX(population) AS population
+      FROM
+      countries
+      GROUP BY
+      continent
+    ) AS max_populations ON
+      c.continent = max_populations.continent AND c.population = max_populations.population) AS candidate
+    JOIN 
+    (SELECT
+      c.continent, MAX(c.population) * 3 AS max_neighbor_population
+        FROM
+          countries c
+        LEFT JOIN
+        (SELECT
+          continent, MAX(population) AS population
+          FROM
+          countries
+          GROUP BY
+          continent
+        ) AS max_populations ON
+          c.continent = max_populations.continent AND c.population = max_populations.population
+          WHERE
+          max_populations.population IS NULL
+          GROUP BY
+          c.continent
+      ) AS neighbors
+      ON candidate.continent = neighbors.continent
+    WHERE candidate.population > neighbors.max_neighbor_population
+
+    
   SQL
 end
+
+
+
+
+
+# SELECT
+# c.continent, MAX(c.area) AS max_neighbor_area
+#   FROM
+#     countries c
+#   LEFT JOIN
+#   (SELECT
+#     continent, MAX(area) AS area
+#     FROM
+#     countries
+#     GROUP BY
+#     continent
+#   ) AS max_areas ON
+#     c.continent = max_areas.continent AND c.area = max_areas.area
+#     WHERE
+#     max_areas.area IS NULL
+#     GROUP BY
+#     c.continent
+    
